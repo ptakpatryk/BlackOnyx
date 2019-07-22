d564fafd5a286db3e058d1917260d877e2e33cff
Alakazam94 <ptaszqq@gmail.com>
2019-07-22 14:49:00 +0200

    animated top with GSAP

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,11 @@
 import '../sass/styles.scss';
+import { TweenMax, Sine } from 'gsap';
 
-
-let test = () => {
-    console.log('Patryk');
-};
-
-
-
-test();
+TweenMax.to('#coral-line', 2.5 , {
+    attr: {
+        d: "M.36,0C45.82,33,117.78,77,214,101.86c94.76,24.5,134.91,10.24,227,37,132.56,38.53,119.2,88.38,219,116,180.65,50,266.76-101.6,432-40,77.65,28.95,103.74,79.26,181,76,64.12-2.69,114.67-40.22,146.23-69.81"
+    },
+    repeat: -1,
+    yoyo: true,
+    ease: Sine.easeInOut
+});
